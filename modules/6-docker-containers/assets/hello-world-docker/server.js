3d5b6fe2df81ad04ea1ac9cd7f53531954f993ea
bourgat <clemence.bourgat-mezin-roux@edu.ece.fr>
2020-11-04 21:54:29 +0100

    moodify message server.js

--- modules/6-docker-containers/assets/hello-world-docker/server.js
+++ modules/6-docker-containers/assets/hello-world-docker/server.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from Docker!');
+  res.send('Hello Clemence from Docker!');
 });
 
 app.listen(PORT);
